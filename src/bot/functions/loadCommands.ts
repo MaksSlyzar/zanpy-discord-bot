d098520eafa6283b98cc88cfba5916507d01154e
MaksSlyzar <maks.slyzar@gmail.com>
2025-04-25 15:32:28 +0300

    create frontend

--- src/bot/functions/loadCommands.ts
+++ src/bot/functions/loadCommands.ts
@@ -1,10 +1,9 @@
 import CommandHello from "../commands/CommandHello";
-import GetAlbionServerStatus from "../commands/GetAlbionServerStatus";
 import RegisterServerStatusChannel from "../commands/RegisterServerStatusChannel";
 import RaidCreate from "../commands/RaidCreate";
 import RaidWriteUser from "../commands/RaidWriteUser";
 import StopRaid from "../commands/StopRaid";
 
 export const loadCommands = () => {
-  return [CommandHello, GetAlbionServerStatus, RegisterServerStatusChannel, RaidCreate, RaidWriteUser, StopRaid];
+  return [CommandHello, RegisterServerStatusChannel, RaidCreate, RaidWriteUser, StopRaid];
 };
